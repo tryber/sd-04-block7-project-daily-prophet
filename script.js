d50437533465581359af2753c5e999fe18a3eabb
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-04-29 19:26:00 -0300

    requirement 10 again

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const element = document.getElementsByClassName('article');
 
 for (let i = 0; i < element.length; i += 1) {
   element[i].addEventListener('click', function (e) {
+    e.target.classList.remove('article-animation');
     e.target.classList.add('article-animation');
   });
 }
