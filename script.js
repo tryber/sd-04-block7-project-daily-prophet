1d1f704915f76ab45620bb5382684a502fd86bd9
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-29 19:40:21 -0300

    Resolvendo o article

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const element = document.getElementsByClassName('article-animation');
 
 for (let i = 0; i < element.length; i += 1) {
-  element[i].addEventListener('click', function (e) {
-    e.target.classList.add('article');
+  element[i].addEventListener('click', function () {
+    this.style.animationPlayState = 'running';
   });
 }
