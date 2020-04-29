509f8ce91d55b9d650cd0ba9dde40ba22d3ec63b
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-29 16:49:02 -0300

    Corrigidos issues.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const lua = document.querySelector('.fly');
 
 function animaLua() {
   lua.style.animationName = 'flying';
-  lua.style.animationDuration = '15s'
+  lua.style.animationDuration = '15s';
   lua.style.animationIterationCount = '3';
   lua.style.animationDirection = 'alternate';
   lua.style.animationFillMode = 'forwards';
