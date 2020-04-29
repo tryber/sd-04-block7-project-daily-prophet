c2e22428598889df4c55b18dae5451c276d602c1
Thabata <dornelasthabata@gmail.com>
2020-04-29 20:42:22 -0300

    CC issues

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const articleAnimation = document.querySelectorAll('.article-animation');
 
 for (let i = 0; i < articleAnimation.length; i += 1) {
   articleAnimation[i].addEventListener('click', function () {
-  articleAnimation[i].style.animationPlayState = 'running';
+    articleAnimation[i].style.animationPlayState = 'running';
   });
 }
