6aefaaf6cd80d4e7bc02d3f900bc40aca08848f7
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-29 18:45:07 -0300

    fixed click function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const articles = document.getElementsByClassName('clicable');
+const articles = document.getElementsByClassName('article-animation');
 const articlesArray = Array.from(articles);
 articlesArray.forEach((article) => {
-  article.addEventListener('click', (e) => (e.target.classList = 'article-animation'));
+  article.addEventListener('click', (e) => (e.target.style.animation = 'article-grow 4s'));
 });
