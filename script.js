a0811b1d6e22276221e41a54092f3c04263d7c30
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-04-29 19:14:43 -0300

    requirement 10 with active

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const element = document.getElementsByClassName('article-animation');
+// const element = document.getElementsByClassName('article-animation');
 
-for (let i = 0; i < element.length; i += 1) {
-  element[i].addEventListener('click', function (e) {
-    e.target.classList.add('article-animation-grow');
-  });
-}
+// for (let i = 0; i < element.length; i += 1) {
+//   element[i].addEventListener('click', function (e) {
+//     e.target.classList.add('article-animation');
+//   });
+// }
