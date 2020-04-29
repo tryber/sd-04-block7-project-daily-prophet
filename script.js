86d1336bcebbb4730c0725e6673ccd46d5fde605
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-04-29 19:00:20 -0300

    trying requirements 10 again

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const element = document.getElementsByClassName('article');
 
 for (let i = 0; i < element.length; i += 1) {
   element[i].addEventListener('click', function (e) {
-    e.target.classList.add('growClass');
+    e.target.classList.add('article-animation');
   });
 }
