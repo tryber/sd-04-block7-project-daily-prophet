4f62f16ac88e404d96f8a84f7022d0354cfecee0
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-29 18:01:52 -0300

    changes for cc

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const articles =  document.getElementsByClassName('clicable')
-const articlesArray = Array.from(articles)
-articlesArray.forEach((article) => article.addEventListener('click', (e) => e.target.classList = "article-animation"))
+const articles = document.getElementsByClassName('clicable');
+const articlesArray = Array.from(articles);
+articlesArray.forEach((article) => {article.addEventListener('click', (e) => e.target.classList = 'article-animation')});
