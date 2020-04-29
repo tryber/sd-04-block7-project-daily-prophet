55b6718c58d5391621a8ebf34ff5800789134d01
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-29 18:17:18 -0300

    cc changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const articles = document.getElementsByClassName('clicable');
 const articlesArray = Array.from(articles);
 articlesArray.forEach((article) => {
-  article.addEventListener('click', (e) => (e.target.classList = 'article-animation'))
+  article.addEventListener('click', (e) => (e.target.classList = 'article-animation'));
 });
