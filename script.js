de425a2321dcd7ed5e8fc20594472aca44eb518e
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-29 16:59:48 -0300

    Corrigidos issues e inserida animationDelay.

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const lua = document.querySelector('.fly');
 function animaLua() {
   lua.style.animationName = 'flying';
   lua.style.animationDuration = '15s';
+  lua.style.animationDelay = '3s';
   lua.style.animationIterationCount = '3';
   lua.style.animationDirection = 'alternate';
   lua.style.animationFillMode = 'forwards';
 }
 
 window.onload = function () {
-  setTimeout(function () {
-    animaLua();
-  }, 3000);
+  animaLua();
 };
