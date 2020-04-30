acff9efdcd2b0d382cc364650917bb91045d92f6
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-29 22:04:57 -0300

    cc fixes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-window.onload = function() {
-  let articles = document.querySelectorAll('article');
-  for (const art of articles) {
-    art.addEventListener('click', function() {
+window.onload = function () {
+  const articles = document.querySelectorAll('article');
+  for (let i = 0; i < articles.length; i += 1) {
+    art = articles[i];
+    art.addEventListener('click', function () {
       art.classList.add('article-animation');
     });
   }
