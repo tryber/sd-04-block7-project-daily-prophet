8dd955543f7bf931c835c6085685219bed7bb2cb
igorgbr <igorgphotoarte@gmail.com>
2020-04-29 23:11:08 -0300

    penultimo

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const sectionNode = document.querySelector('.section');
 const articleNode = document.querySelector('.article');
-
-console.log(articleNode)
-
-articleNode.addEventListener('click', function(){
-    console.log('teste')
-    articleNode.className = '';
-    articleNode.className = 'article-animation';
-})
+articleNode.addEventListener('click', function () {
+  articleNode.className = '';
+  articleNode.className = 'article-animation';
+});
