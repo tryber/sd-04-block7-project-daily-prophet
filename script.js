2bb6d69236a979a0086792338708b084bf1389eb
procopio420 <lucas.p@gec.inatel.br>
2020-04-29 21:49:56 -0300

    ordem alfabetica

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const article = document.getElementsByTagName('article');
 
-article[0].addEventListener('focusout', () => {
-  article[0].offsetWidth;
-  article[0].classList.remove('article-animation');
-})
