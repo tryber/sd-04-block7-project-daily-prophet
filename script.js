9806ac5c617857211dc638e6c46f51c4a8151459
orlando-messias <programadorlando@gmail.com>
2020-04-30 13:12:51 -0300

    tranitions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 window.onload = () => {
   document.getElementById('image-fly').classList.add('fly');
+  document.getElementsByClassName('title')[0].style.transform = 'scale(1.3)';
+  document.getElementById('link').style.transform = 'skewX(-15deg)';
 };
