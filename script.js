c0106675ba5c93427439ac6720c63a353cc44f0d
danilossousa <danilosantossousa@gmail.com>
2020-04-30 11:23:51 -0300

    v0.10.0.3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const articleAnimation = document.querySelector('.article1');
 
-function enlargeArticle(e) {
-  e.target.classList.add('article-animation');
+function enlargeArticle() {
+  articleAnimation.classList.add('article-animation');
 }
 
 articleAnimation.addEventListener('click', enlargeArticle);
