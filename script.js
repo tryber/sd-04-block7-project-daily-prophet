6c5780dfdd1b93b480134df67c0e4c2585951bd3
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-29 22:12:16 -0300

    cc fixes 4........................

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+function addEvent(elem) {
+  elem.addEventListener('click', function () {
+    elem.classList.add('article-animation');
+  });
+}
+
 window.onload = function () {
   const articles = document.querySelectorAll('article');
   for (let i = 0; i < articles.length; i += 1) {
     addEvent(articles[i]);
   }
 };
-
-function addEvent(elem) {
-  elem.addEventListener('click', function () {
-    elem.classList.add('article-animation');
-  });
-}
