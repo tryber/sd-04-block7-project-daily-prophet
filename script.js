33505ee95a2a2f7072e41f48efb92bf3dec574d6
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-30 02:14:22 -0300

    adicionado article e animação item 10

---
+++ script.js
@@ -0,0 +1,10 @@
+// *********************** declaração de variaveis ********************************************
+const article = document.querySelector('article');
+// ************************ funções ***********************************************************
+function addClass() {
+  article.classList.add('article-animation');
+}
+
+window.onload = function () {
+  article.addEventListener('click', addClass);
+};
