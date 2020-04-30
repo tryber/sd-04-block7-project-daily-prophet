c14450be92ca6d5d1d5f2e943fcf939061dfc424
Julio Cesar <stininmatic@hotmail.com>
2020-04-30 01:07:09 -0300

    Criando animação correção

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const coruja = document.querySelector('.fly');
 coruja.style.animation = 'flying 7000ms ease-in-out';
 coruja.style.animationDelay = '3000ms';
-coruja.style.animationIterationCount = '3'
-
+coruja.style.animationIterationCount = '3';
