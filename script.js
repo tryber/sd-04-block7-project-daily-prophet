5591e4641c33e46ed9bae48433f269b21dde8af2
Julio Cesar <stininmatic@hotmail.com>
2020-04-30 00:46:29 -0300

    Criando animação

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const coruja = document.querySelector('.fly');
-coruja.style.animation = "flying 7000ms ease-in-out";
+coruja.style.animation = 'flying 7000ms ease-in-out';
 coruja.style.animationDelay = '3000ms';
