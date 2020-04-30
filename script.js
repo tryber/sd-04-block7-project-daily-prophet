e521a11c6712630b592dc77897b8a01e6786dee7
juniomelos <you@example.com>
2020-04-30 14:22:49 -0300

    Requirement - remaking efect style running - CC issue 1

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const elementAnimation = document.getElementsByClassName('article-animation');
 
 for (let i = 0; i < elementAnimation.length; i += 1) {
   elementAnimation[i].addEventListener('click', function () {
-  elementAnimation[i].style.animationPlayState = 'running';
+    elementAnimation[i].style.animationPlayState = 'running';
   });
 }
