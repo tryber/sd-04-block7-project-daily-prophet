2723f59cc91660b3d7661603e9f3ab1a9e502bc8
Julio Cesar <stininmatic@hotmail.com>
2020-04-30 17:41:54 -0300

    Teste atualizaçõs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const coruja = document.querySelector('.fly');
-coruja.style.animation = 'flying 7000ms ease-in-out';
+coruja.style.animation = 'flying 7000ms ease-in-out forwards';
 coruja.style.animationDelay = '3000ms';
 coruja.style.animationIterationCount = '3';
 coruja.style.animationDirection = 'alternate';
