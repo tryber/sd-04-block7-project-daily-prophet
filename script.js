fd2f760a88bf5ebd7af14665d1268b2dae9c5ff2
orlando-messias <programadorlando@gmail.com>
2020-05-01 17:49:12 -0300

    code climate issues solved

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   document.getElementById('image-fly').classList.add('fly');
   document.getElementsByClassName('title')[0].style.transform = 'scale(1.1)';
-  document.getElementById('link').style.transform = 'skew(-15deg, 0deg)';
+  document.getElementById('link').style.transform = 'skew(0deg, 0deg)';
 };
 
 document.querySelector('.container-first-section .subtitle').addEventListener('click', (e) => {
