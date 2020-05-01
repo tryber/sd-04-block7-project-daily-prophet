dc56db5b5bf3db247b1d323ad3340ea1d2d1d66d
Julio Cesar <stininmatic@hotmail.com>
2020-05-01 19:44:19 -0300

    Efeito artigo

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ coruja.style.animation = 'flying 7000ms ease-in-out forwards';
 coruja.style.animationDelay = '3000ms';
 coruja.style.animationIterationCount = '3';
 coruja.style.animationDirection = 'alternate';
+
+const artigo = document.querySelector('.article-animation');
+artigo.addEventListener('click', () => {
+  artigo.style.animation = 'zoom 4000ms ease-in';
+});
