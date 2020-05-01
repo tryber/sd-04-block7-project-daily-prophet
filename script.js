5a53b628d1388e3846317227f092c20af111ac2b
Douglas <douglaza@gmail.com>
2020-05-01 20:55:14 -0300

    4th try to fix the last requirement

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const article = document.getElementById('lorem-article');
 
 function animaElem() {
-  article.style.animation = 'article-grow 4s';
+  // article.style.animation = 'article-grow 4s';
+  article.style.fontWeight = 'bold';
+  article.style.transform = 'scale(1.5)';
 }
 
 article.addEventListener('click', animaElem);
