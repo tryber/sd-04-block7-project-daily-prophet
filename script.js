d09415900dacfbf4f3fc17d9577816f36c6acd1d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-05-01 18:59:44 -0300

    fix/code climate

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const menuButton = document.querySelector('.menu-button');
 const menuLine = document.querySelector('.menu-line');
-// const nav = document.querySelector('.menu-line');
 const navLinks = document.querySelector('.nav-links');
-const navbar = document.querySelector('.navbar');
 
 menuButton.addEventListener('click', () => {
   navLinks.classList.toggle('nav-links-active');
   menuLine.classList.toggle('menu-line-active');
-  document.body.style.overflow = document.body.style.overflow === 
+  document.body.style.overflow = document.body.style.overflow ===
   'hidden' ? 'initial' : 'hidden';
 });
