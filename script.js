fbb9ab24a0a9ee4718b038c02a0944920661dd4c
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-05-01 18:39:50 -0300

    feature/added articles - added footer

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const menuButton = document.querySelector('.menu-button');
 const menuLine = document.querySelector('.menu-line');
 // const nav = document.querySelector('.menu-line');
 const navLinks = document.querySelector('.nav-links');
+const navbar = document.querySelector('.navbar');
 
 menuButton.addEventListener('click', () => {
   navLinks.classList.toggle('nav-links-active');
   menuLine.classList.toggle('menu-line-active');
+  document.body.style.overflow = document.body.style.overflow === 
+  'hidden' ? 'initial' : 'hidden';
 });
