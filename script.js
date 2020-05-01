de4d3dacdf240a69e10c25df31045e7300b99e3a
orlando-messias <programadorlando@gmail.com>
2020-05-01 13:59:02 -0300

    add second section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
   document.getElementById('image-fly').classList.add('fly');
-  document.getElementsByClassName('title')[0].style.transform = 'scale(1.3)';
+  document.getElementsByClassName('title')[0].style.transform = 'scale(1.1)';
   document.getElementById('link').style.transform = 'skew(-15deg, 0deg)';
 };
