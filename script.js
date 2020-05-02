c526740a562b8c8c7fa06a50fd61f83d57a7895a
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-02 15:23:37 -0300

    arrumando o js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const article = document.getElementsByClassName('class-animation');
 for (let i = 0; i < article.length; i += 1) {
-  article[i].addEventeListener('click', function () {
+  article[i].addEventListener('click', function () {
     article[i].style.animationPlayState = 'running';
   });
 }
