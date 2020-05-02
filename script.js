890d1d8655764942c0c0d036a309a959963f69b9
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-02 15:15:59 -0300

    testando o cc

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const article = document.getElementsByClassName('class-animation')[0];
-article.onclick = function () {
-  this.style.fontWeight = '700';
-};
+const article = document.getElementsByClassName('class-animation');
+for(let i = 0; i < article.length; i += 1){
+  article[i].addEventeListener('click', function () {
+    article[i].style.animationPlayState = 'running';
+  })
+}
+
