bfe068298cd5903de0890b34811b7737617c9230
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-02 15:18:11 -0300

    try again

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const article = document.getElementsByClassName('class-animation');
-for(let i = 0; i < article.length; i += 1){
+for (let i = 0; i < article.length; i += 1) {
   article[i].addEventeListener('click', function () {
     article[i].style.animationPlayState = 'running';
-  })
+  });
 }
-
