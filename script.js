049a656bcf7eb5a9e4f8a996831a94611d184938
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-05-02 13:43:07 -0300

    feature/added article transition

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const menuButton = document.querySelector('.menu-button');
 const menuLine = document.querySelector('.menu-line');
 const navLinks = document.querySelector('.nav-links');
+const articles = document.querySelectorAll('article');
 
 menuButton.addEventListener('click', () => {
   navLinks.classList.toggle('nav-links-active');
   menuLine.classList.toggle('menu-line-active');
   document.body.style.overflow = document.body.style.overflow ===
   'hidden' ? 'initial' : 'hidden';
+});
+
+articles.forEach((article) => {
+  article.addEventListener('click', () => {
+    article.classList.toggle('clicked-article');
+  });
 });
