a1458bba0b576ecae8d962284eade391ab8edbaa
frederico-campello <fredfmc33@gmail.com>
2020-05-02 18:29:13 -0300

    Atualizando o projeto

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 window.onload = function () {
-  // const artigos = document.querySelectorAll('p');
-  // function adicionaClasse() {
-  //   for (let i = 0; i < artigos.length; i += 1) {
-  //     artigos[i].classList.add('article-animation');
-  //   }
-  // }
-  // artigos[0].addEventListener('click', adicionaClasse);
+
 };
