e5e0e4d3e88858d4f8233d2ec0166455a86a2cff
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-05-01 21:12:00 -0300

    item 10 adicionado event listner para animação dos artigos

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// // *********************** declaração de variaveis ********************************************
-// const article = document.querySelector('article');
-// // ************************ funções ***********************************************************
-// function addClass() {
-//   article.classList.add('article-animation');
-// }
+// *********************** declaração de variaveis ********************************************
+const article = document.querySelectorAll('article');
+// ************************ funções ***********************************************************
 
-// window.onload = function () {
-//   article.addEventListener('click', addClass);
-// };
+window.onload = () => {
+  for (let i = 0; i < article.length; i += 1) {
+    article[i].addEventListener('click', () => {
+      article[i].style.animationPlayState = 'running';
+    });
+  }
+};
