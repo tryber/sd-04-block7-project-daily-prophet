ad11969d6b8aecad47ba5ead8556d2d7754cbe5f
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-05-03 14:46:41 -0300

    first final version!

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// const rocket = document.querySelector('.rocket-complete');
-// const rocket2 = document.querySelector('.rocket');
 const starsTd = document.querySelectorAll('.star');
 
 function randomTd() {
   const randomIndex = Math.floor(Math.random() * starsTd.length);
   return randomIndex;
 }
-// function randomDelay () {
-//   const randomDelay = Math.floor(Math.random()*2 + 2.999999999)
-//   return randomDelay;
-// }
-// const animationsArray = ['starsFlashing', 'starsFlashing2', 'starsFlashing3'];
 
 let saveArray = [];
 
@@ -36,37 +29,5 @@ setInterval(function () {
 const articleDiv = document.querySelector('article');
 
 articleDiv.addEventListener('click', function () {
-  articleDiv.classList.add('article-animation');
+  articleDiv.style.animationPlayState = 'running';
 });
-// function starsDisappearing() {
-//   for (let i = 0; i < saveArray.length; i += 1) {
-//     starsTd[i].className = 'star starsFlashing';
-//   }
-// //   console.log(saveArray);
-// }
-
-// document.querySelector('.container-layout').addEventListener('mouseover', function() {
-//   starsFlashing();
-// })
-// console.log(saveArray);
-
-// rocket.addEventListener('animationend', bounceTransition);
-
-
-// function bounceTransition() {
-//   // const rect = rocket.getBoundingClientRect();
-//   // rocket.style.setProperty('left', `${rect.left}px`);
-//   // rocket.style.setProperty('top', `${rect.top}px`);
-//   rocket2.classList.add('big')
-// }
-
-
-// function gettingStyle() {
-//   // const cssStyle = rocket.offsetLeft;
-//   // const cssStyle2 = rocket.offsetTop;
-//   // console.log(cssStyle2, cssStyle);
-//   const rect = rocket.getBoundingClientRect();
-
-//   console.log("popup.getBoundingClientRect(): \n" + "x: " + rect.left + "\ny: " + rect.top);
-// }
-// rocket.addEventListener('mouseover', gettingStyle)
