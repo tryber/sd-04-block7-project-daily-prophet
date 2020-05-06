a12da6319822ea66aeb8a601701892d8093d6106
Julianafatsil <julianafatsil@gmail.com>
2020-05-06 02:22:18 -0300

    remove js

--- script.js
+++
@@ -1,9 +0,0 @@
-const CURRENTDATE = document.getElementById('current-date');
-
-function GetDate() {
-  const MONTH = ['January', 'February', 'March', 'abril', 'May', 'junho', 'agosto', 'outubro', 'November', 'Dezember'];
-  const DATE = new Date();
-  return `${MONTH[DATE.getMonth()]}, ${DATE.getDate()}, ${DATE.getFullYear()}`;
-}
-
-CURRENTDATE.innerText = GetDate();
