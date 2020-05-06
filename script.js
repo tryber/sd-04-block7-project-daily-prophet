0e7158527142c403a5996a3fb24c3ca8e1ef78a0
cacobribeiro <cacobribeiro@gmail.com>
2020-05-06 11:04:01 -0300

    fix transition duplicate

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   for (let i = 0; i < articles.length; i += 1) {
     articles[i].addEventListener('click', function () {
-      articles[i].classList.toggle('article-animation');
+      articles[i].classList.toggle('growUp');
     });
   }
 };
