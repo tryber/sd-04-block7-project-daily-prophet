da11f0b0bad3d1d898c7d820ff761ed49f5b27f6
djonata-dias <diasdjonata@gmail.com>
2020-05-06 03:31:19 -0300

    ajustando animações

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = () => {
-  const articles = document.getElementsByTagName('article');
-  for (let i = 0; i < articles.length; i += 1) {
-    articles[i].addEventListener('click', () => {
-      articles[i].classList.toggle('article-animation');
-    });
-  }
+//   const articles = document.getElementsByTagName('article');
+//   for (let i = 0; i < articles.length; i += 1) {
+//     articles[i].addEventListener('click', () => {
+//       articles[i].classList.toggle('article-animation');
+//     });
+//   }
 };
