f79dc954192d8b98ff9637c4dc312700868fb163
Julianafatsil <julianafatsil@gmail.com>
2020-05-07 03:13:15 -0300

    adding styling

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const CURRENTDATE = document.getElementById('current-date');
 function GetDate() {
   const MONTH = ['January', 'February', 'March', 'abril', 'May', 'junho', 'agosto', 'outubro', 'November', 'Dezember'];
   const DATE = new Date();
-  return `Day: ${DATE.getDate()} of ${MONTH[DATE.getMonth()]} of ${DATE.getFullYear()}`;
+  return `${MONTH[DATE.getMonth()]}, ${DATE.getDate()} ${DATE.getFullYear()}`;
 }
 
 CURRENTDATE.innerText = GetDate();
