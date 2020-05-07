b37fe8ba4b03f93f4b0a6efc3149b09dc9ef318a
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-07 11:29:35 -0300

    leves ajustes 2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const article = document.getElementsByClassName('article-animation');
 for (let i = 0; i < article.length; i += 1) {
   article[i].addEventListener('click', function () {
-    article[i].style.animationPlayState = 'paused';
     article[i].style.animationPlayState = 'running';
   });
 }
