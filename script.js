51c65aed93349685e91c2754db392ecd8355edee
samueap <samueap@gmail.com>
2020-05-07 12:12:04 -0300

    try fix CC

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const triggeringAnimation = document.getElementsByClassName('article-animation');
+// const triggeringAnimation = document.getElementsByClassName('article-animation');
 
-for (let i = 0; i < triggeringAnimation.length; i += 1){
-	triggeringAnimation[i].addEventListener('click', function(){
-		triggeringAnimation[i].style.animationPlayState = 'running';
-	});
-}
+// for (let i = 0; i < triggeringAnimation.length; i += 1){
+// 	triggeringAnimation[i].addEventListener('click', function(){
+// 		triggeringAnimation[i].style.animationPlayState = 'running';
+// 	});
+// }
